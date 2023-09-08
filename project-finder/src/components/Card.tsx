4ad7258f1def89167ae96893f6dde13ac1b50314
unknown <justfitra11@gmail.com>
2023-09-08 22:32:43 +0700

    style: formatting card

--- project-finder/src/components/Card.tsx
+++ project-finder/src/components/Card.tsx
@@ -29,33 +29,35 @@ interface ProjectProps {
 
 const ProjectCard: React.FC<ProjectProps> = ({ project, username }) => {
   return (
-      <Card hoverable className="card-style">
-        <div className="card-container">
-          <Title code level={3} className="card-title">
+    <Card hoverable className="card-style">
+      <div className="card-container">
+        <div className="card-header">
+          <Title level={3} code className="card-title">
             {project.name}
           </Title>
-          <Text keyboard>ID: {project.id}</Text>
-          <div className="span-container">
-            <p>
-              <span className="span-desc">Desc:</span>
-              {project.description}
-            </p>
-            <p>
-              <span className="span-lang">Lang:</span>
-              {project.language}
-            </p>
-            <p>
-              <span className="span-updated">Updated:</span>
-              {formatDate(project.updated_at)}
-            </p>
-          </div>
+          <Text keyboard className="card-project-id">ID: {project.id}</Text>
         </div>
-        <Link to={`/project/${username}/${project.name}`}>
-          <Button className="card-button" type="text">
-            Read More
-          </Button>
-        </Link>
-      </Card>
+        <div className="span-container">
+          <p>
+            <span className="span-desc">Desc:</span>
+            {project.description}
+          </p>
+          <p>
+            <span className="span-lang">Lang:</span>
+            {project.language}
+          </p>
+          <p>
+            <span className="span-updated">Updated:</span>
+            {formatDate(project.updated_at)}
+          </p>
+        </div>
+      </div>
+      <Link to={`/project/${username}/${project.name}`}>
+        <Button className="card-button" type="text">
+          Read More
+        </Button>
+      </Link>
+    </Card>
   );
 };
 
